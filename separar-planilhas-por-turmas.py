c67690aa29dc07a0641bfad3a5efbe9a18eb81c5
Davidson Santana <59514471+davidsonsantana89@users.noreply.github.com>
2023-01-26 10:43:05 -0300

    Update separar-planilhas-por-turmas.py

--- separar-planilhas-por-turmas.py
+++ separar-planilhas-por-turmas.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
-
-import os
 import pandas as pd
 import streamlit as st
 
@@ -12,42 +8,17 @@ import streamlit as st
 # In[3]:
 
 
-caminho = r'C:\Users\david\OneDrive\ESCOLA\ETE LAL\CONTROLE DE FREQUÊNCIAS E NOTAS\2021\ELETIVAS\ANUAL\TURMAS\geral-anual.xlsx'
+caminho = https://drive.google.com/drive/folders/13TF1k1BbhINlrpLfQSTaZhw0wJJXs0WW?usp=sharing
+
+print(caminho)
+
+# df = pd.read_excel(caminho)
 
 
-# In[4]:
+#turmas = df['TURMA'].unique()
 
 
-df = pd.read_excel(caminho)
+#for turma in turmas:
+#    df[df['TURMA'] == turma].to_excel(r"C:\Users\david\OneDrive\ESCOLA\ETE LAL\CONTROLE DE FREQUÊNCIAS E NOTAS\2021\ELETIVAS\ANUAL\TURMAS" + '\\' + f'{turma}.xlsx', index=False)
 
-
-# In[5]:
-
-
-df.head()
-
-
-# In[6]:
-
-
-turmas = df['TURMA'].unique()
-
-
-# In[10]:
-
-
-for turma in turmas:
-    df[df['TURMA'] == turma].to_excel(r"C:\Users\david\OneDrive\ESCOLA\ETE LAL\CONTROLE DE FREQUÊNCIAS E NOTAS\2021\ELETIVAS\ANUAL\TURMAS" + '\\' + f'{turma}.xlsx', index=False)
-
-
-# In[11]:
-
-
-df.to_excel(r'C:\Users\david\OneDrive\ESCOLA\ETE LAL\CONTROLE DE FREQUÊNCIAS E NOTAS\2021\ELETIVAS\ANUAL\TURMAS\geral-anual.xlsx', index=False)
-
-
-# In[ ]:
-
-
-
-
+# df.to_excel(r'C:\Users\david\OneDrive\ESCOLA\ETE LAL\CONTROLE DE FREQUÊNCIAS E NOTAS\2021\ELETIVAS\ANUAL\TURMAS\geral-anual.xlsx', index=False)
